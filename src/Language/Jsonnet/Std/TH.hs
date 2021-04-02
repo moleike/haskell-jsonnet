362e86925c9f08b14f15f2ba8bd1ca2a16162e3e
Alexandre Moreno <alexmorenocano@gmail.com>
2021-04-02 11:21:49 +0800

    add pathRelativeToCabalPackage

--- src/Language/Jsonnet/Std/TH.hs
+++ src/Language/Jsonnet/Std/TH.hs
@@ -8,13 +8,15 @@ import Language.Haskell.TH
 import Language.Haskell.TH.Quote ()
 import Language.Haskell.TH.Syntax (addDependentFile)
 import Language.Jsonnet.TH (parse)
+import TH.RelativePaths (pathRelativeToCabalPackage)
 
 stdlibPath :: String
 stdlibPath = "stdlib/std.jsonnet"
 
 mkStdlib :: Q Exp
 mkStdlib = do
+  fp <- pathRelativeToCabalPackage stdlibPath
+  addDependentFile fp
   src <- runIO (TIO.readFile stdlibPath)
   ast <- parse stdlibPath src
-  addDependentFile stdlibPath
   pure ast
